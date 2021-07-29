3c8940a486f58ae2869cd7efd43ab2845ba7c01f
蘇健豪 <jenhausu@osensetech.com>
2021-07-29 20:36:33 +0800

    Initial Commit

---
+++ SixThinkingHats/SixThinkingHatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixThinkingHatsApp.swift
+//  SixThinkingHats
+//
+//  Created by 蘇健豪 on 2021/7/29.
+//
+
+import SwiftUI
+
+@main
+struct SixThinkingHatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
