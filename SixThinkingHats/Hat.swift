3e51243f0e4c7bd19dd88a7ab5bc44a0cee474d7
蘇健豪 <jenhausu@osensetech.com>
2021-07-29 20:38:27 +0800

    feat: create Hat enum

---
+++ SixThinkingHats/Hat.swift
@@ -0,0 +1,17 @@
+//
+//  Hat.swift
+//  SixThinkingHats
+//
+//  Created by 蘇健豪 on 2021/7/29.
+//
+
+import Foundation
+
+enum Hat {
+    case white
+    case red
+    case black
+    case yellow
+    case greed
+    case blue
+}
